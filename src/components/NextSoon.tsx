aad6012702866fda70bd64c66706a06abfd66c7e
Jaziel Rodriguez <aguafapolla@gmail.com>
2023-04-04 18:47:07 -0500

    Currencies app added to projects

--- src/components/NextSoon.tsx
+++ src/components/NextSoon.tsx
@@ -1,11 +1,27 @@
 import styles from "../styles/NextSoon.module.css";
 import { fontPoppins } from "@/utils/font";
 import { useTranslation } from "react-i18next";
+import Image from "next/image";
+import Container from "./Container";
+import currencies from "../images/currecies.png";
 const NextSoon = () => {
   const { t } = useTranslation("translations");
   return (
     <div className={styles.next}>
-      <h1 className={fontPoppins}>{t("comingSoon")}</h1>
+      <Container>
+        <h1 className={fontPoppins}>{t("myWork")}</h1>
+        <div className={styles.works}>
+          <div className={styles.cover}>
+            <a href="http://currencies-pink.vercel.app/" target="_blank">
+              <Image
+                src={currencies}
+                alt="Jaziel Rodriguez project - Currencies App"
+                fill={true}
+              />
+            </a>
+          </div>
+        </div>
+      </Container>
     </div>
   );
 };
